449d51085f3e1face5608cddddaef4451f8c739a
ralvescosta <rafael.rac.mg@gmail.com>
2021-01-19 20:53:08 -0300

    refactor: home page

--- src/pages/Home/view/index.tsx
+++ src/pages/Home/view/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./styles.css"
 
 import {TodoItem} from '../../../components/TodoItem'
 
 export const HomeView = () => {
   return (
-    <TodoItem />
+    <div className="todo-list-container">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
+    </div>
   )
 }
