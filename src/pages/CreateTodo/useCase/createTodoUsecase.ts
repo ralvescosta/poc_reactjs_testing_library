575a30c8c34c565f8d485b635ce67519f07eac83
ralvescosta <rafael.rac.mg@gmail.com>
2021-01-20 21:43:09 -0300

    refactor: create todo view

--- src/pages/CreateTodo/useCase/createTodoUsecase.ts
+++ src/pages/CreateTodo/useCase/createTodoUsecase.ts
@@ -10,7 +10,7 @@ export class CreateTodoUsecase implements ICreateTodoUsecase {
       const todo = TodoModel.create({ name: todoName, description: todoDescription, priority })
       return todo
     } catch (err) {
-      throw err.message
+      return err.message
     }
   }
 }
