b5fbb335164333a7f50a269573ff2881006867f5
Rafael Alves <rafael.rac.mg@gmail.com>
2021-02-01 07:04:54 -0300

    feat: repository item component

--- src/pages/SearchRepository/view/RepositoryItems/index.tsx
+++ src/pages/SearchRepository/view/RepositoryItems/index.tsx
@@ -9,8 +9,30 @@ type Props = {
 
 export const RepositoryItem = ({ repository }: Props) => {
   return (
-    <li >
-      <h1>{repository.fullName}</h1>
+    <li className="repository-item-container">
+      <img className="repository-item-avatar" src={repository.ownerAvatarUrl} />
+
+      <div className="repository-item-content">
+        <strong>{repository.fullName}</strong>
+
+        <div className="repository-item-content-p-overflow">
+          <p>{repository.description}</p>
+        </div>
+
+        <div className="repository-item-content-indicators">
+
+          <div>
+            <strong>STARS: </strong><span>{repository.stargazersCount}</span>
+          </div>
+          <div>
+            <strong>FORKS: </strong><span>{repository.forks}</span>
+          </div>
+          <div>
+            <strong>ISSUES: </strong><span>{repository.openIssues}</span>
+          </div>
+
+        </div>
+      </div>
     </li>
   )
 }
