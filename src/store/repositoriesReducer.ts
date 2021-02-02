f4fb12709448d38cd303bbcdabfa1965eceb316a
Rafaerl <rafael.rac.mg@gmail.com>
2021-02-02 13:34:15 -0300

    feat: reducer and action

---
+++ src/store/repositoriesReducer.ts
@@ -0,0 +1,33 @@
+import { Types } from './repositoriesActions'
+import { RepositoryModel } from '../models/repositoryModel'
+
+export type TTodoReducer = {
+  repositories: RepositoryModel[];
+};
+const INITIAL_STATE: TTodoReducer = {
+  repositories: []
+}
+
+type ActionType = {
+  type: string;
+  payload: any;
+};
+
+export const repositoriesReducer = (state = INITIAL_STATE, action: ActionType) => {
+  switch (action.type) {
+    case Types.ADD_REPOSITORY:
+      return {
+        ...state,
+        repositories: [...state.repositories, action.payload]
+      }
+
+    case Types.DELETE_REPOSITORY:
+      return {
+        ...state,
+        repositories: state.repositories.filter(repository => repository.id !== action.payload.id)
+      }
+
+    default:
+      return state
+  }
+}
