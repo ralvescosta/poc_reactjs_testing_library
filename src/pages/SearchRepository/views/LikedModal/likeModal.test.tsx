286920ed3b0a73562ff53660687ced87da41bd07
Luis Gustavo <devluispereira@gmail.com>
2021-02-08 08:02:49 -0300

    testando repository item

--- src/pages/SearchRepository/views/LikedModal/likeModal.test.tsx
+++ src/pages/SearchRepository/views/LikedModal/likeModal.test.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
-
-import { cleanup, fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react'
 
 import { useLikedModalViewModel } from '../../viewModels/likedModalViewModel'
 import { LikedModal } from './index'
 
 import { LikedModalContextProvider } from '../../context/likedModalContext'
-const Wrapper:React.FC = () => {
+const Wrapper: React.FC = () => {
   const viewModel = useLikedModalViewModel()
-  return <LikedModal viewModel={viewModel}/>
+  return <LikedModal viewModel={viewModel} />
 }
 
 describe('test da view ', () => {
-  beforeAll(
-    () => {
-      jest.clearAllMocks()
-      cleanup()
-    }
-  )
+  beforeAll(() => {
+    jest.clearAllMocks()
+    cleanup()
+  })
   const repositoryFake = {
     id: 1,
     description: 'Repository Teste',
@@ -29,21 +27,24 @@ describe('test da view ', () => {
     releasesUrl: 'relesed',
     stargazersCount: 5
   }
-
   it('Testar se o componete está renderizando', () => {
-    const { getByText } = render(
+    render(
       <LikedModalContextProvider repositoryValue={repositoryFake} modalDisplayValue={'block'} >
         <Wrapper/>
       </LikedModalContextProvider>
     )
-    expect(getByText(repositoryFake.fullName)).toBe(repositoryFake.fullName)
-    expect(getByText(repositoryFake.description)).toBe(repositoryFake.description)
+    waitFor(() => {
+      expect('Repository Test React Testing Library').toBeInTheDocument()
+    })
   })
 
   it('Testar se o componente irá fechar ao clicar no × ', () => {
     const { getByText, getByTestId } = render(
-      <LikedModalContextProvider repositoryValue={repositoryFake} modalDisplayValue={'block'} >
-        <Wrapper/>
+      <LikedModalContextProvider
+        repositoryValue={repositoryFake}
+        modalDisplayValue={'block'}
+      >
+        <Wrapper />
       </LikedModalContextProvider>
     )
     const modal = getByTestId('modal')
--- src/pages/SearchRepository/views/RepositoryItems/RepositoryItens.test.tsx
+++ src/pages/SearchRepository/views/RepositoryItems/RepositoryItens.test.tsx
@@ -3,27 +3,29 @@ import { LikedModalContextProvider } from '../../context/likedModalContext'
 import { RepositoryItem } from './index'
 import { useRepositoryItemModalViewModel } from '../../viewModels/repositoryItemViewModel'
 import { IRepositoryItemViewModel } from '../../interfaces/irepositoryItemViewModel'
-import { cleanup, render } from '@testing-library/react'
-const repositoryFake = {
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { RepositoryModel } from '../../../../models/repositoryModel'
+const repositoryFake:any = {
   id: 1,
   description: 'Repository Teste',
-  forks: 5,
+  forks: 10,
   fullName: 'Repository Test React Testing Library',
-  openIssues: 5,
+  openIssues: 3,
   ownerAvatarUrl: 'null',
   ownerId: 5,
   releasesUrl: 'relesed',
-  stargazersCount: 5
+  stargazersCount: 200
+}
+
+const viewModelStub:IRepositoryItemViewModel = {
+  openModal: jest.fn(),
+  repository: repositoryFake
 }
 
 const Wrapper = () => {
-  const repositoryStub :IRepositoryItemViewModel = {
-    openModal: jest.fn(),
-    repository: repositoryFake
-  }
-  const viewModel = useRepositoryItemModalViewModel(repositoryStub)
   return (
-<RepositoryItem viewModel={viewModel}/>
+<RepositoryItem viewModel={viewModelStub}/>
   )
 }
 describe('Testando renderização do Card', () => {
@@ -34,10 +36,33 @@ describe('Testando renderização do Card', () => {
     }
   )
   it('should ', () => {
-    const {} = render(
+    const { queryByText } = render(
     <LikedModalContextProvider repositoryValue={repositoryFake} modalDisplayValue={'none'} >
     <Wrapper/>
     </LikedModalContextProvider>
+
     )
+    const starts = queryByText(repositoryFake.stargazersCount)
+    const forks = queryByText(repositoryFake.forks)
+    const issues = queryByText(repositoryFake.openIssues)
+    const fullName = queryByText(repositoryFake.fullName)
+
+    expect(fullName).toBeInTheDocument()
+    expect(forks).toBeInTheDocument()
+    expect(starts).toBeInTheDocument()
+    expect(issues).toBeInTheDocument()
+  })
+
+  it('testar abertura modal', () => {
+    const { queryByText } = render(
+      <LikedModalContextProvider repositoryValue={repositoryFake} modalDisplayValue={'none'} >
+      <Wrapper/>
+      </LikedModalContextProvider>)
+
+    const LIKE = queryByText('LIKE')
+
+    fireEvent.click(LIKE as any)
+
+    expect(viewModelStub.openModal).toHaveBeenCalledTimes(1)
   })
 })
